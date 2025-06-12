98aa4fdf8bb6161bd500b8f0d6681802d7d5cf00
Krish Parikh <kp3516@stern.nyu.edu>
2025-06-12 10:59:01 -0400

    Fix host and port binding

--- app.py
+++ app.py
@@ -16,5 +16,5 @@ def handle_booking():
     result = book_bobst_room(username, password, time)
     return jsonify(result)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
